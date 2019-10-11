b6cd8069fabc4be46a0bc5f8861e13779585d950
Hector <hectorberryg@gmail.com>
2019-10-11 13:16:20 +0200

    API routes added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://root:toor>@cluster0-kshud.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://root:toor@cluster0-kshud.mongodb.net/test?retryWrites=true&w=majority'
 };
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const items = require('./routes/api/items');
 
 const app = express();
 
 //Bodyparser Middleware
 app.use(bodyParser.json());
 
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to DB
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err));
+
+//Use routes
+app.use('/api/items', items)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
